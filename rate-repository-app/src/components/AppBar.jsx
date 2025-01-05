4347fcdfb023fef3171320bceac698a275c05864
oskah <oskah@uef.fi>
2025-01-05 11:20:42 +0200

    10.6-10.7 react router native

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,26 +1,39 @@
-import { View, StyleSheet, Pressable } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { Link } from "react-router-native";
 import Constants from "expo-constants";
 import theme from "../theme";
-import Text from './Text';
+import Text from "./Text";
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     paddingBottom: 10,
     backgroundColor: theme.colors.appBarBackground,
-    justifyContent: 'center',
-    paddingLeft: 16, 
+    justifyContent: "center",
+    paddingLeft: 16,
     paddingVertical: 10,
+  },
+  link: {
+    marginRight: 16,
+    justifyContent: "center",
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-        <Pressable /*onPress={onPressFunction}*/>
-      <Text color ="appBarText" fontSize="heading" fontWeight="bold" >Repositories
-      </Text>
-      </Pressable>
+      <ScrollView horizontal>
+        <Link to="/" style={styles.link}>
+          <Text color="appBarText" fontSize="heading" fontWeight="bold">
+            Repositories
+          </Text>
+        </Link>
+        <Link to="/signIn" style={styles.link}>
+          <Text color="appBarText" fontSize="subheading">
+            Sign in
+          </Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
