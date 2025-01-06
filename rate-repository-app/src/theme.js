179677254d140e80825aa970c40f68c3ed988ce2
oskah <oskah@uef.fi>
2025-01-06 08:11:31 +0200

    10.10 platform-specific font

--- rate-repository-app/src/theme.js
+++ rate-repository-app/src/theme.js
@@ -1,3 +1,5 @@
+import { Platform} from 'react-native';
+
 const theme = {
     colors: {
       textPrimary: '#24292e',
@@ -11,9 +13,12 @@ const theme = {
       subheading: 16,
       heading: 20,
     },
-    fonts: {
-      main: 'System',
-    },
+    fonts: Platform.select({ 
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System',
+    })
+    ,
     fontWeights: {
       normal: '400',
       bold: '700',
